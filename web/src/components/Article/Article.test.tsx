c4e61e3c7317a9745801d929c2d9db77bf77f0a1
dhythm <yuta.okada.20@gmail.com>
2022-07-23 22:20:50 +0900

    working in chapter.6

--- web/src/components/Article/Article.test.tsx
+++ web/src/components/Article/Article.test.tsx
@@ -1,4 +1,6 @@
-import { render, screen } from '@redwoodjs/testing'
+import { render, screen, waitFor } from '@redwoodjs/testing'
+
+import { standard } from 'src/components/CommentsCell/CommentsCell.mock'
 
 import Article from './Article'
 
@@ -13,11 +15,20 @@ const ARTICLE = {
 }
 
 describe('Article', () => {
-  it('renders successfully', () => {
+  it('renders a blog post', () => {
     render(<Article article={ARTICLE} />)
 
     expect(screen.getByText(ARTICLE.title)).toBeInTheDocument()
     expect(screen.getByText(ARTICLE.body)).toBeInTheDocument()
+  })
+
+  it('renders comments when displaying a full blog post', async () => {
+    const comment = standard().comments[0]
+    render(<Article article={ARTICLE} />)
+
+    await waitFor(() =>
+      expect(screen.getByText(comment.body)).toBeInTheDocument()
+    )
   })
 
   it('renders a summary of a blog post', () => {
@@ -29,5 +40,14 @@ describe('Article', () => {
         'Neutra tacos hot chicken prism raw denim, put a bird on it enamel pin post-ironic vape cred DIY. Str...'
       )
     ).toBeInTheDocument()
+  })
+
+  it('does not render comments when displaying a summary', async () => {
+    const comment = standard().comments[0]
+    render(<Article article={ARTICLE} summary={true} />)
+
+    await waitFor(() =>
+      expect(screen.queryByText(comment.body)).not.toBeInTheDocument()
+    )
   })
 })
